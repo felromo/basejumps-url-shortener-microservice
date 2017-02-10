b94e66395313af97f0cfa31c11d52d4b52961e8c
Tyrantz <tyrantwarzhip@gmail.com>
2017-02-10 03:59:01 -0600

    creates routes for links that dont have the protocol

--- index.js
+++ index.js
@@ -5,9 +5,34 @@ var router = express.Router();
 
 var PORT = process.env.PORT || 8080;
 
+var shortenedUrls = [];
+
+function shortenUrl(url) {
+    var tmpUrl;
+    do {
+        tmpUrl = Math.floor((Math.random()*100) + 1);
+    } while (shortenedUrls.includes(tmpUrl))
+    return tmpUrl;
+}
+
+function createRoute(url, shortUrl) {
+    router.get('/' + shortUrl, function (req, res) {
+        res.redirect('http://' + url);
+    });
+}
+
 router.get('/', function (req, res) {
+    res.send('Url required');
+});
+
+router.get('/new/:url?', function (req, res) {
+    var url = req.params.url;
+    var shortUrl = shortenUrl(url);
+    shortenedUrls.push(shortUrl);
+    createRoute(url, shortUrl);
     res.json({
-        hello: 'world!'
+        url: url,
+        shortUrl: shortUrl
     });
 });
 
