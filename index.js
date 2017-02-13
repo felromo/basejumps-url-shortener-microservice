5d40a95c5de7ccce5658e7412ae1ab6c5292fbc7
Tyrantz <tyrantwarzhip@gmail.com>
2017-02-13 17:36:51 -0600

    returns basic errors and instruction uses when used incorrectly, checks for protocol

--- index.js
+++ index.js
@@ -17,16 +17,34 @@ function shortenUrl(url) {
 
 function createRoute(url, shortUrl) {
     router.get('/' + shortUrl, function (req, res) {
-        res.redirect('http://' + url);
+        res.redirect(url);
     });
 }
 
 router.get('/', function (req, res) {
+    res.send('Use: placeholder/new/http://link');
+});
+
+router.param('url', function (req, res, next, url) {
+    console.log('this is whats passed to me', url);
+    // validates that there is a protocol
+    if (/^https?:\/\//.test(url)) {
+        req.url = url;
+    } else {
+        req.url = "";
+    }
+    next();
+});
+
+router.get('/new', function (req, res) {
     res.send('Url required');
 });
 
-router.get('/new/:url?', function (req, res) {
-    var url = req.params.url;
+router.get('/new/:url(*)', function (req, res) {
+    var url = req.url;
+    if (url.length == 0) {
+        res.send('Url must contain either http:// or https://');
+    }
     var shortUrl = shortenUrl(url);
     shortenedUrls.push(shortUrl);
     createRoute(url, shortUrl);
